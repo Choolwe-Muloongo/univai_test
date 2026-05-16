22e747187263e7306fbf810f37d41df5789fb798
Choolwe DM3 <voltech0506@gmail.com>
2026-05-16 11:24:30 +0200

    Add short course review route

---
+++ src/app/admin/short-courses/review/page.tsx
@@ -0,0 +1,5 @@
+import { ShortCourseReviewClient } from '@/components/admin/short-course-review-client';
+
+export default function ShortCourseReviewPage() {
+  return <ShortCourseReviewClient />;
+}
