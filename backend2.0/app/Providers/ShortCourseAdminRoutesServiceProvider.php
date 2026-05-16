63c4f0cf60d99a97faade9b175f837ca87d230e8
Choolwe DM3 <voltech0506@gmail.com>
2026-05-16 14:26:16 +0200

    Register short course question bank admin routes

--- backend2.0/app/Providers/ShortCourseAdminRoutesServiceProvider.php
+++ backend2.0/app/Providers/ShortCourseAdminRoutesServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Controllers\Api\AdminExamQuestionsController;
 use App\Http\Controllers\Api\AdminShortCourseInsightsController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
@@ -12,6 +13,13 @@ class ShortCourseAdminRoutesServiceProvider extends ServiceProvider
     {
         Route::middleware(['api', 'session.auth', 'access:admin.academic'])
             ->prefix('api/admin')
-            ->get('/short-courses/insights', [AdminShortCourseInsightsController::class, 'index']);
+            ->group(function () {
+                Route::get('/short-courses/insights', [AdminShortCourseInsightsController::class, 'index']);
+                Route::get('/short-courses/question-bank', [AdminExamQuestionsController::class, 'index']);
+                Route::post('/short-courses/question-bank', [AdminExamQuestionsController::class, 'store']);
+                Route::post('/short-courses/question-bank/bulk', [AdminExamQuestionsController::class, 'bulkStore']);
+                Route::patch('/short-courses/question-bank/{examQuestion}', [AdminExamQuestionsController::class, 'update']);
+                Route::delete('/short-courses/question-bank/{examQuestion}', [AdminExamQuestionsController::class, 'destroy']);
+            });
     }
 }
