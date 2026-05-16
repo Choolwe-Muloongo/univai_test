4b6fac90807582b8c8768d69a2620e1d4fdd9893
Choolwe DM3 <voltech0506@gmail.com>
2026-05-16 13:37:37 +0200

    Register student short course API routes

---
+++ backend2.0/app/Providers/ShortCourseStudentRoutesServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Controllers\Api\ShortCourseController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class ShortCourseStudentRoutesServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Route::middleware(['api', 'session.auth', 'access:student.portal'])
+            ->prefix('api/students/me/short-courses')
+            ->group(function () {
+                Route::post('/{courseId}/enroll', [ShortCourseController::class, 'enroll']);
+                Route::get('/{courseId}/progress', [ShortCourseController::class, 'progress']);
+                Route::post('/{courseId}/lessons/{lessonId}/complete', [ShortCourseController::class, 'completeLesson']);
+                Route::post('/{courseId}/exam/submit', [ShortCourseController::class, 'submitExam']);
+                Route::post('/{courseId}/certificate/pay', [ShortCourseController::class, 'payCertificate']);
+                Route::get('/{courseId}/certificate', [ShortCourseController::class, 'certificate']);
+            });
+    }
+}
