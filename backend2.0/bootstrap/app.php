701c00fa0f0893738bb04db155089ab0028ba463
Choolwe DM3 <voltech0506@gmail.com>
2026-05-17 01:17:35 +0200

    Type API exception debug renderer for Laravel

--- backend2.0/bootstrap/app.php
+++ backend2.0/bootstrap/app.php
@@ -31,7 +31,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function ($e, $request) {
+        $exceptions->render(function (\Throwable $e, \Illuminate\Http\Request $request) {
             if (!$request->is('api/*')) {
                 return null;
             }
