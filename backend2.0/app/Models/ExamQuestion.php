3a31ac49a6e2307d7b81d71f0fe0b87accbd04c8
Choolwe DM3 <voltech0506@gmail.com>
2026-05-16 14:23:59 +0200

    Expand exam question model for short course quiz bank

--- backend2.0/app/Models/ExamQuestion.php
+++ backend2.0/app/Models/ExamQuestion.php
@@ -8,14 +8,22 @@ class ExamQuestion extends Model
 {
     protected $fillable = [
         'course_id',
+        'lesson_id',
         'semester',
         'question',
+        'question_type',
         'options',
         'answer',
+        'explanation',
         'difficulty',
+        'time_seconds',
+        'source',
+        'tags',
     ];
 
     protected $casts = [
         'options' => 'array',
+        'tags' => 'array',
+        'time_seconds' => 'integer',
     ];
 }
