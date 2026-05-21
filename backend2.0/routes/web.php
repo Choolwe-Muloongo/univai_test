23abc4458c3cf1a90abc2994c1e846e93d03f6a2
Choolwe DM3 <voltech0506@gmail.com>
2026-05-21 10:07:19 +0200

    Register short course access routes

--- backend2.0/routes/web.php
+++ backend2.0/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\ShortCourseAccessController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['api', 'session.auth'])->prefix('api')->group(function () {
+    Route::get('/students/me/short-courses/{courseId}/access-plans', [ShortCourseAccessController::class, 'plans']);
+    Route::post('/students/me/short-courses/{courseId}/access-plans/purchase', [ShortCourseAccessController::class, 'purchase']);
+    Route::get('/students/me/short-courses/bundles', [ShortCourseAccessController::class, 'bundlePlans']);
+    Route::post('/students/me/short-courses/bundles/purchase', [ShortCourseAccessController::class, 'purchaseBundle']);
 });
