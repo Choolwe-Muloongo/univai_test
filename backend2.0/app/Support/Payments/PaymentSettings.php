4ed985e41f10ffa044dac4ac89b5ecdff892785f
Choolwe DM3 <voltech0506@gmail.com>
2026-05-22 08:51:26 +0200

    Add payment mode helpers

--- backend2.0/app/Support/Payments/PaymentSettings.php
+++ backend2.0/app/Support/Payments/PaymentSettings.php
@@ -6,16 +6,40 @@ use App\Models\PaymentSetting;
 
 class PaymentSettings
 {
+    public const MODE_LIVE = 'live';
+    public const MODE_LENCO_TEST = 'lenco_test';
+    public const MODE_LOCAL_TEST = 'local_test';
+
     public static function current(): PaymentSetting
     {
         return PaymentSetting::query()->firstOrCreate([], [
-            'lenco_collections_enabled' => false,
-            'test_mode_message' => 'Lenco collections are disabled. Payments activate in test mode.',
+            'lenco_collections_enabled' => true,
+            'payment_mode' => self::MODE_LIVE,
+            'show_test_payments_in_admin' => true,
+            'test_mode_message' => 'Payments are running in test mode. Records are labelled as test payments.',
         ]);
+    }
+
+    public static function mode(): string
+    {
+        $mode = (string) (self::current()->payment_mode ?: self::MODE_LIVE);
+        return in_array($mode, [self::MODE_LIVE, self::MODE_LENCO_TEST, self::MODE_LOCAL_TEST], true)
+            ? $mode
+            : self::MODE_LIVE;
     }
 
     public static function lencoCollectionsEnabled(): bool
     {
-        return (bool) self::current()->lenco_collections_enabled;
+        return self::mode() !== self::MODE_LOCAL_TEST && (bool) self::current()->lenco_collections_enabled;
+    }
+
+    public static function paymentsAreTest(): bool
+    {
+        return self::mode() !== self::MODE_LIVE;
+    }
+
+    public static function adminShowsTestPayments(): bool
+    {
+        return (bool) self::current()->show_test_payments_in_admin;
     }
 }
