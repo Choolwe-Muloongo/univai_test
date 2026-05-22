dde520e66f153d399a3e998128893902847b710e
Choolwe DM3 <voltech0506@gmail.com>
2026-05-22 08:50:33 +0200

    Add payment mode fields to setting model

--- backend2.0/app/Models/PaymentSetting.php
+++ backend2.0/app/Models/PaymentSetting.php
@@ -8,11 +8,14 @@ class PaymentSetting extends Model
 {
     protected $fillable = [
         'lenco_collections_enabled',
+        'payment_mode',
+        'show_test_payments_in_admin',
         'test_mode_message',
         'updated_by',
     ];
 
     protected $casts = [
         'lenco_collections_enabled' => 'boolean',
+        'show_test_payments_in_admin' => 'boolean',
     ];
 }
