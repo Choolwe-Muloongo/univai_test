21064d4f2e2929c965cb0508f7b0e3fbb3a12d58
Choolwe DM3 <voltech0506@gmail.com>
2026-05-22 09:10:12 +0200

    Capture backend exceptions in beta reports

--- backend2.0/bootstrap/app.php
+++ backend2.0/bootstrap/app.php
@@ -31,6 +31,14 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->report(function (\Throwable $e) {
+            try {
+                \App\Support\BetaReports\BetaErrorReporter::capture($e, request());
+            } catch (\Throwable) {
+                // Error reporting must never break the app.
+            }
+        });
+
         $exceptions->render(function (\Throwable $e, \Illuminate\Http\Request $request) {
             if (!$request->is('api/*')) {
                 return null;
