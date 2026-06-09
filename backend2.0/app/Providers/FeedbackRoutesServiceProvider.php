e3e1a8c531b962e688cd9d605f0950b23ba5d4f7
Choolwe DM3 <voltech0506@gmail.com>
2026-06-09 08:08:02 +0200

    Register feedback board routes

---
+++ backend2.0/app/Providers/FeedbackRoutesServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Controllers\Api\AdminFeedbackController;
+use App\Http\Controllers\Api\FeedbackController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class FeedbackRoutesServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Route::middleware(['api', 'session.auth', 'access:student.portal'])
+            ->prefix('api')
+            ->group(function () {
+                Route::get('/feedback', [FeedbackController::class, 'index']);
+                Route::post('/feedback', [FeedbackController::class, 'store']);
+                Route::get('/feedback/{feedbackPost}', [FeedbackController::class, 'show']);
+                Route::post('/feedback/{feedbackPost}/vote', [FeedbackController::class, 'vote']);
+                Route::delete('/feedback/{feedbackPost}/vote', [FeedbackController::class, 'unvote']);
+                Route::post('/feedback/{feedbackPost}/comments', [FeedbackController::class, 'comment']);
+                Route::post('/app-rating', [FeedbackController::class, 'rating']);
+                Route::get('/app-rating/summary', [FeedbackController::class, 'ratingSummary']);
+            });
+
+        Route::middleware(['api', 'session.auth', 'access:admin.portal'])
+            ->prefix('api/admin')
+            ->group(function () {
+                Route::get('/feedback', [AdminFeedbackController::class, 'index']);
+                Route::get('/feedback/stats', [AdminFeedbackController::class, 'stats']);
+                Route::patch('/feedback/{feedbackPost}', [AdminFeedbackController::class, 'update']);
+                Route::post('/feedback/{feedbackPost}/comments', [AdminFeedbackController::class, 'comment']);
+            });
+    }
+}
