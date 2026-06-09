3d645958de84b2339aa54ab1acf2cf2f99951f40
Choolwe DM3 <voltech0506@gmail.com>
2026-06-09 08:05:33 +0200

    Add feedback comment model

---
+++ backend2.0/app/Models/FeedbackComment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FeedbackComment extends Model
+{
+    protected $fillable = [
+        'feedback_post_id',
+        'user_key',
+        'author_name',
+        'body',
+        'is_admin_reply',
+    ];
+
+    protected $casts = [
+        'is_admin_reply' => 'boolean',
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(FeedbackPost::class, 'feedback_post_id');
+    }
+}
