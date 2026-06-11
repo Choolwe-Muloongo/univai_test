92bc1d774c74cf8619bdc755ad4259fe8848bd4c
Choolwe DM3 <voltech0506@gmail.com>
2026-06-11 18:16:08 +0200

    Register gamification ranking routes

--- backend2.0/routes/web.php
+++ backend2.0/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ShortCourseAccessController;
 use App\Http\Controllers\Api\ShortCourseLeaderboardController;
+use App\Http\Controllers\Api\StudentGamificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,6 +10,15 @@ Route::get('/', function () {
 });
 
 Route::middleware(['api', 'session.auth'])->prefix('api')->group(function () {
+    Route::get('/students/me/gamification', [StudentGamificationController::class, 'state']);
+    Route::get('/students/me/daily-quests', [StudentGamificationController::class, 'dailyQuests']);
+    Route::post('/students/me/learning-events', [StudentGamificationController::class, 'recordEvent']);
+    Route::get('/students/me/rewards', [StudentGamificationController::class, 'rewards']);
+    Route::post('/students/me/rewards/redeem', [StudentGamificationController::class, 'redeem']);
+    Route::get('/students/me/leaderboard/activity', [StudentGamificationController::class, 'leaderboard']);
+    Route::get('/students/me/mistakes', [StudentGamificationController::class, 'mistakes']);
+    Route::post('/students/me/mistakes/{id}/review', [StudentGamificationController::class, 'reviewMistake']);
+
     Route::get('/students/me/short-courses/{courseId}/access-plans', [ShortCourseAccessController::class, 'plans']);
     Route::post('/students/me/short-courses/{courseId}/access-plans/purchase', [ShortCourseAccessController::class, 'purchase']);
     Route::get('/students/me/short-courses/bundles', [ShortCourseAccessController::class, 'bundlePlans']);
