0e9564395ba05c60cea3f8be69bc81076aa18d16
Choolwe DM3 <voltech0506@gmail.com>
2026-06-12 07:41:19 +0200

    Remove duplicated API routes from web routes

--- backend2.0/routes/web.php
+++ backend2.0/routes/web.php
@@ -1,27 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\ShortCourseAccessController;
-use App\Http\Controllers\Api\ShortCourseLeaderboardController;
-use App\Http\Controllers\Api\StudentGamificationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::middleware(['api', 'session.auth'])->prefix('api')->group(function () {
-    Route::get('/students/me/gamification', [StudentGamificationController::class, 'state']);
-    Route::get('/students/me/daily-quests', [StudentGamificationController::class, 'dailyQuests']);
-    Route::post('/students/me/learning-events', [StudentGamificationController::class, 'recordEvent']);
-    Route::get('/students/me/rewards', [StudentGamificationController::class, 'rewards']);
-    Route::post('/students/me/rewards/redeem', [StudentGamificationController::class, 'redeem']);
-    Route::get('/students/me/leaderboard/activity', [StudentGamificationController::class, 'leaderboard']);
-    Route::get('/students/me/mistakes', [StudentGamificationController::class, 'mistakes']);
-    Route::post('/students/me/mistakes/{id}/review', [StudentGamificationController::class, 'reviewMistake']);
-
-    Route::get('/students/me/short-courses/{courseId}/access-plans', [ShortCourseAccessController::class, 'plans']);
-    Route::post('/students/me/short-courses/{courseId}/access-plans/purchase', [ShortCourseAccessController::class, 'purchase']);
-    Route::get('/students/me/short-courses/bundles', [ShortCourseAccessController::class, 'bundlePlans']);
-    Route::post('/students/me/short-courses/bundles/purchase', [ShortCourseAccessController::class, 'purchaseBundle']);
-    Route::get('/students/me/short-courses/{courseId}/leaderboard', [ShortCourseLeaderboardController::class, 'show']);
 });
