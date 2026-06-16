a201c98c9c6f30354b429910cee3df34204f8aec
Choolwe DM3 <voltech0506@gmail.com>
2026-06-16 11:14:57 +0200

    Load affiliate patch routes after legacy routes

--- backend2.0/bootstrap/providers.php
+++ backend2.0/bootstrap/providers.php
@@ -3,10 +3,10 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CorsPatchServiceProvider::class,
-    App\Providers\AffiliatePatchRoutesServiceProvider::class,
     App\Providers\AcademicRoutesServiceProvider::class,
     App\Providers\ShortCourseAdminRoutesServiceProvider::class,
     App\Providers\ShortCourseStudentRoutesServiceProvider::class,
     App\Providers\FeedbackRoutesServiceProvider::class,
     App\Providers\AdminUserCareRoutesServiceProvider::class,
+    App\Providers\AffiliatePatchRoutesServiceProvider::class,
 ];
